894545b24b3bfb6fcc9377923d531a260209713b
Deniz Kızılaslan <kizilaslandeniz@gmail.com>
2022-07-25 12:08:26 +0300

    added file

---
+++ ExtensionMethods/ExtensionMethod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class ExtensionMethod
+    {
+        public static bool isOddNumber(this int number)
+        {
+            return number % 2 != 0;
+        }
+        public static bool isEvenNumber(this int number)
+        {
+            return !isOddNumber(number);
+        }
+
+        public static string ConvertToTL(this int number)
+        {
+            decimal value = Convert.ToDecimal(number);
+            return $"{value} TL";
+        }
+        public static int multiplybyTen(this int number)
+        {
+            return number * 10;
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(2);
+            list.Add(1);
+            list.Add(3);
+            list.Add(4);
+            Console.WriteLine("Lütfen bir sayı giriniz.");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool odd = number.isOddNumber();
+            bool even = number.isEvenNumber();
+            int multiplybyTen = number.multiplybyTen();
+
+            Console.WriteLine("Tek Sayı :" + odd );
+            Console.WriteLine("Çift Sayı :" + even );
+            Console.WriteLine("10 Katı :" + multiplybyTen.ToString() );
+            Console.WriteLine("TL :" + number.ConvertToTL());
+
+            Console.ReadLine();
+        }
+    }
+}
